7353504c95a9a1798aa6ee77d9a68f123093bf45
WaterB1rd <142914283+WaterB1rd@users.noreply.github.com>
2023-08-27 18:24:30 -0500

    Added my name and major onto hello.c.

--- hello.c
+++ hello.c
@@ -4,7 +4,9 @@
 int main(int argc, char **argv) {
 
     printf("Elijah Larson!!!!\n");
-    printf("Electrical Engineering!!!!!\n");
+    printf("My major is Electrical Engineering!!!!!\n");
+    printf("And I am Lane Vette!\n");
+    printf("My major is also electrical Engineering!\n"); 
 
 return 0;
 }
